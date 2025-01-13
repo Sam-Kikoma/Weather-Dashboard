3b13af4f9eddc7e1579a098ae50810b8a4721bd5
Sam Kikoma <kikomasamson@gmail.com>
2025-01-13 23:45:07 +0100

    feat: weather forecast response handing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import Forecast from "./components/Forecast";
 import Highlight from "./components/Highlight";
-import More from "./components/More";
 import Search from "./components/Search";
 import Temperature from "./components/Temperature";
 
 const App = () => {
 	const key = import.meta.env.VITE_OPENWEATHER_APIKEY;
 	const [searchValue, setSearchValue] = useState("");
+	const [forecast, setForecast] = useState([]);
 	const [response, setResponse] = useState([]);
 	const fetchData = async (value: string): Promise<void> => {
 		try {
@@ -15,11 +15,15 @@ const App = () => {
 			if (!res.ok) throw new Error("Failed to fetch data");
 			const parsedRes = await res.json();
 			setResponse(parsedRes);
+			const futureForecast = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=${key}`);
+			const moreParsedRes = await futureForecast.json();
+			setForecast(moreParsedRes);
 		} catch (error) {
 			console.error("Error fetching data:", error);
 			setResponse([]);
 		}
 	};
+	console.log(forecast);
 
 	return (
 		<div className="min-h-screen bg-gray-900 p-4 sm:p-6 lg:p-8 flex flex-col gap-6 font-sans text-gray-100">
@@ -42,15 +46,14 @@ const App = () => {
 				</div>
 
 				<div className="col-span-1 sm:col-span-2 lg:col-span-3 bg-gray-800 shadow-lg rounded-xl p-6">
-					<Highlight />
+					{response.length === 0 ? (
+						<h1 className="text-gray-400 text-center">Enter a location</h1>
+					) : (
+						<Highlight data={response} />
+					)}
 				</div>
-
-				<div className="col-span-1 sm:col-span-1 lg:col-span-2 bg-gray-800 shadow-lg rounded-xl p-6">
-					<More />
-				</div>
-
-				<div className="col-span-1 sm:col-span-1 lg:col-span-2 bg-gray-800 shadow-lg rounded-xl p-6">
-					<Forecast />
+				<div className="col-span-1 sm:col-span-1 lg:col-span-4 bg-gray-800 shadow-lg rounded-xl p-6">
+					<Forecast data={forecast} />
 				</div>
 			</div>
 		</div>
